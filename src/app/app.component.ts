d9dc9bd5e0d727e0d7a7b55c3f0845582d86502e
techiediaries <techiediaries9@gmail.com>
2020-01-17 15:57:30 +0100

    add httpclient

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'first-web-app-v2';
+  constructor(private httpClient: HttpClient){}
+  
+  ngOnInit(): void {
+    this.httpClient.get(" http://localhost:3000/products").subscribe(
+    (data) =>{
+      console.log(data);
+    }
+    );
+  }  
 }
